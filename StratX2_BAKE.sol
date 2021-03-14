91f3112fb8ad23fb494c33fe2ce090102350c693
autofarm.network <75677769+autofarm-network@users.noreply.github.com>
2021-03-15 06:56:21 +0800

    Create StratX2_BAKE.sol

---
+++ StratX2_BAKE.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "./StratX2.sol";
+
+interface IBakeryswapFarm {
+    function deposit(address _pair, uint256 _amount) external;
+
+    function withdraw(address _pair, uint256 _amount) external;
+}
+
+contract StratX2_BAKE is StratX2 {
+    constructor(
+        address[] memory _addresses,
+        uint256 _pid,
+        bool _isCAKEStaking,
+        bool _isSameAssetDeposit,
+        bool _isAutoComp,
+        address[] memory _earnedToAUTOPath,
+        address[] memory _earnedToToken0Path,
+        address[] memory _earnedToToken1Path,
+        address[] memory _token0ToEarnedPath,
+        address[] memory _token1ToEarnedPath,
+        uint256 _controllerFee,
+        uint256 _buyBackRate,
+        uint256 _entranceFeeFactor,
+        uint256 _withdrawFeeFactor
+    ) public {
+        wbnbAddress = _addresses[0];
+        govAddress = _addresses[1];
+        autoFarmAddress = _addresses[2];
+        AUTOAddress = _addresses[3];
+
+        wantAddress = _addresses[4];
+        token0Address = _addresses[5];
+        token1Address = _addresses[6];
+        earnedAddress = _addresses[7];
+
+        farmContractAddress = _addresses[8];
+        pid = _pid;
+        isCAKEStaking = _isCAKEStaking;
+        isSameAssetDeposit = _isSameAssetDeposit;
+        isAutoComp = _isAutoComp;
+
+        uniRouterAddress = _addresses[9];
+        earnedToAUTOPath = _earnedToAUTOPath;
+        earnedToToken0Path = _earnedToToken0Path;
+        earnedToToken1Path = _earnedToToken1Path;
+        token0ToEarnedPath = _token0ToEarnedPath;
+        token1ToEarnedPath = _token1ToEarnedPath;
+
+        controllerFee = _controllerFee;
+        rewardsAddress = _addresses[10];
+        buyBackRate = _buyBackRate;
+        buyBackAddress = _addresses[11];
+        entranceFeeFactor = _entranceFeeFactor;
+        withdrawFeeFactor = _withdrawFeeFactor;
+
+        transferOwnership(autoFarmAddress);
+    }
+
+    function _farm() internal override {
+        uint256 wantAmt = IERC20(wantAddress).balanceOf(address(this));
+        wantLockedTotal = wantLockedTotal.add(wantAmt);
+        IERC20(wantAddress).safeIncreaseAllowance(farmContractAddress, wantAmt);
+
+        IBakeryswapFarm(farmContractAddress).deposit(wantAddress, wantAmt);
+    }
+
+    function _unfarm(uint256 _wantAmt) internal override {
+        IBakeryswapFarm(farmContractAddress).withdraw(wantAddress, _wantAmt);
+    }
+}
