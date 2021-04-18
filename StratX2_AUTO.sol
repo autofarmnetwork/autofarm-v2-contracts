3d0c829895e0e7bca57c2cd0250ef8903dbb6022
autofarm.network <75677769+autofarm-network@users.noreply.github.com>
2021-04-18 23:08:51 +0800

    Update StratX2_AUTO.sol
    Added withdrawal delay

--- StratX2_AUTO.sol
+++ StratX2_AUTO.sol
@@ -5,6 +5,18 @@ pragma solidity 0.6.12;
 import "./StratX2.sol";
 
 contract StratX2_AUTO is StratX2 {
+    address[] public users;
+    mapping(address => uint256) public userLastDepositedTimestamp;
+    uint256 public minTimeToWithdraw; // 604800 = 1 week
+    uint256 public minTimeToWithdrawUL = 1209600; // 2 weeks
+
+    event minTimeToWithdrawChanged(
+        uint256 oldMinTimeToWithdraw,
+        uint256 newMinTimeToWithdraw
+    );
+
+    event earned(uint256 oldWantLockedTotal, uint256 newWantLockedTotal);
+
     constructor(
         address[] memory _addresses,
         uint256 _pid,
@@ -54,12 +66,93 @@ contract StratX2_AUTO is StratX2 {
         transferOwnership(autoFarmAddress);
     }
 
+    function deposit(address _userAddress, uint256 _wantAmt)
+        public
+        override
+        onlyOwner
+        nonReentrant
+        whenNotPaused
+        returns (uint256)
+    {
+        if (userLastDepositedTimestamp[_userAddress] == 0) {
+            users.push(_userAddress);
+        }
+        userLastDepositedTimestamp[_userAddress] = block.timestamp;
+
+        IERC20(wantAddress).safeTransferFrom(
+            address(msg.sender),
+            address(this),
+            _wantAmt
+        );
+
+        uint256 sharesAdded = _wantAmt;
+        if (wantLockedTotal > 0 && sharesTotal > 0) {
+            sharesAdded = _wantAmt
+                .mul(sharesTotal)
+                .mul(entranceFeeFactor)
+                .div(wantLockedTotal)
+                .div(entranceFeeFactorMax);
+        }
+        sharesTotal = sharesTotal.add(sharesAdded);
+
+        wantLockedTotal = IERC20(AUTOAddress).balanceOf(address(this));
+
+        return sharesAdded;
+    }
+
+    function withdraw(address _userAddress, uint256 _wantAmt)
+        public
+        override
+        onlyOwner
+        nonReentrant
+        returns (uint256)
+    {
+        require(
+            (userLastDepositedTimestamp[_userAddress].add(minTimeToWithdraw)) <
+                block.timestamp,
+            "too early!"
+        );
+
+        require(_wantAmt > 0, "_wantAmt <= 0");
+
+        uint256 sharesRemoved = _wantAmt.mul(sharesTotal).div(wantLockedTotal);
+        if (sharesRemoved > sharesTotal) {
+            sharesRemoved = sharesTotal;
+        }
+        sharesTotal = sharesTotal.sub(sharesRemoved);
+
+        if (withdrawFeeFactor < withdrawFeeFactorMax) {
+            _wantAmt = _wantAmt.mul(withdrawFeeFactor).div(
+                withdrawFeeFactorMax
+            );
+        }
+
+        // if (isAutoComp) {
+        //     _unfarm(_wantAmt);
+        // }
+
+        uint256 wantAmt = IERC20(wantAddress).balanceOf(address(this));
+        if (_wantAmt > wantAmt) {
+            _wantAmt = wantAmt;
+        }
+
+        if (wantLockedTotal < _wantAmt) {
+            _wantAmt = wantLockedTotal;
+        }
+
+        wantLockedTotal = wantLockedTotal.sub(_wantAmt);
+
+        IERC20(wantAddress).safeTransfer(autoFarmAddress, _wantAmt);
+
+        return sharesRemoved;
+    }
+
     function _farm() internal override {}
 
     function _unfarm(uint256 _wantAmt) internal override {}
 
     function earn() public override whenNotPaused {
-        require(isAutoComp, "!isAutoComp");
+        // require(isAutoComp, "!isAutoComp");
         if (onlyGov) {
             require(msg.sender == govAddress, "!gov");
         }
@@ -70,19 +163,43 @@ contract StratX2_AUTO is StratX2 {
 
         uint256 earnedAmt = IERC20(earnedAddress).balanceOf(address(this));
 
-        earnedAmt = distributeFees(earnedAmt);
+        // earnedAmt = distributeFees(earnedAmt);   // Not need to distribute fees again. Already done.
 
+        IERC20(earnedAddress).safeApprove(uniRouterAddress, 0);
+        IERC20(earnedAddress).safeIncreaseAllowance(
+            uniRouterAddress,
+            earnedAmt
+        );
         _safeSwap(
             uniRouterAddress,
             earnedAmt,
             slippageFactor,
             earnedToAUTOPath,
-            buyBackAddress,
+            address(this),
             block.timestamp.add(600)
         );
 
         lastEarnBlock = block.number;
 
+        uint256 wantLockedTotalOld = wantLockedTotal;
+
         wantLockedTotal = IERC20(AUTOAddress).balanceOf(address(this));
+
+        emit earned(wantLockedTotalOld, wantLockedTotal);
     }
+
+    function setMinTimeToWithdraw(uint256 newMinTimeToWithdraw)
+        public
+        onlyAllowGov
+    {
+        require(newMinTimeToWithdraw <= minTimeToWithdrawUL, "too high");
+        emit minTimeToWithdrawChanged(minTimeToWithdraw, newMinTimeToWithdraw);
+        minTimeToWithdraw = newMinTimeToWithdraw;
+    }
+
+    function userLength() public view returns (uint256) {
+        return users.length;
+    }
+
+    receive() external payable {}
 }
